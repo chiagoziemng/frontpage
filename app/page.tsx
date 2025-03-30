383177ab6ed7f3f741a08ef0e7bd8f772bbc3d05
chiagoziemng <chiagoziemjamesng@gmail.com>
2025-03-30 19:14:16 +0100

    fest: footer fix

--- app/page.tsx
+++ app/page.tsx
@@ -402,7 +402,7 @@ export default function Home() {
       </section>
 
       <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
-        <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
+        {/* <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
           <h1 className="text-3xl md:text-5xl font-medium ">
             <Image
               src={"/logo/logo.webp"}
@@ -416,10 +416,10 @@ export default function Home() {
           <p className="text-left  text-xl  text-gray-500">
             admin@birdsoftware.ca
           </p>
-        </div>
+        </div> */}
 
         <div className="flex md:justify-center gap-x-4 mt-10">
-          © 2025 Bird. All Rights Reserved.
+          © 2025 OMAR AND BLASIUS LIMITED - All Rights Reserved.
           <Link href="/" className="text-blue-500">
             Privacy Policy
           </Link>
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
-      <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
+      {/* <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
         <h1 className="text-3xl md:text-5xl font-medium ">
           <Image
             src={"/logo/logo.webp"}
@@ -18,10 +18,10 @@ const Footer = () => {
         <p className="text-left  text-xl  text-gray-500">
           admin@birdsoftware.ca
         </p>
-      </div>
+      </div> */}
 
       <div className="flex md:justify-center gap-x-4 mt-10">
-        © 2024 Bird. All Rights Reserved.
+        © 2025 OMAR AND BLASIUS LIMITED - All Rights Reserved.
         <Link href="/" className="text-blue-500">
           Privacy Policy
         </Link>
